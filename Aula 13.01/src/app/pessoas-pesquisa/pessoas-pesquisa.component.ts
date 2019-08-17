c8dac12fdb2029c9980ec0d487a0ca0c8887329b
Raulzito311 <raulmff@gmail.com>
2019-08-17 17:02:57 -0300

    Criando o protótipo do formulário de cadastro de lançamentos

---
+++ Aula 13.01/src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoas-pesquisa',
+  templateUrl: './pessoas-pesquisa.component.html',
+  styleUrls: ['./pessoas-pesquisa.component.css']
+})
+export class PessoasPesquisaComponent {
+
+  cols = [
+    { field: 'nome', header: 'Nome' },
+    { field: 'cidade', header: 'Cidade' },
+    { field: 'estado', header: 'Estado' },
+    { field: 'ativo', header: 'Status' }
+  ];
+
+  pessoas = [
+    { nome: 'Manoel Pinheiro', cidade: 'Uberlândia', estado: 'MG', ativo: true },
+    { nome: 'Sebastião da Silva', cidade: 'São Paulo', estado: 'SP', ativo: false },
+    { nome: 'Carla Souza', cidade: 'Florianópolis', estado: 'SC', ativo: true },
+    { nome: 'Luís Pereira', cidade: 'Curitiba', estado: 'PR', ativo: true },
+    { nome: 'Vilmar Andrade', cidade: 'Rio de Janeiro', estado: 'RJ', ativo: false },
+    { nome: 'Paula Maria', cidade: 'Uberlândia', estado: 'MG', ativo: true }
+  ];
+
+}
