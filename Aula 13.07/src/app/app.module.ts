5ad71d9a5c90c2fa215d89b607ececf393831ba4
Raulzito311 <raulmff@gmail.com>
2019-08-18 23:08:19 -0300

    Adicionando campo CEP com máscara (componente InputMask)

---
+++ Aula 13.07/src/app/app.module.ts
@@ -0,0 +1,50 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+
+import {InputTextModule} from 'primeng/inputtext';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+import {TooltipModule} from 'primeng/tooltip';
+import {CalendarModule} from 'primeng/calendar';
+import {SelectButtonModule} from 'primeng/selectbutton';
+import {DropdownModule} from 'primeng/dropdown';
+import {InputMaskModule} from 'primeng/inputmask';
+
+import { AppComponent } from './app.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LancamentosPesquisaComponent,
+    NavbarComponent,
+    PessoasPesquisaComponent,
+    LancamentoCadastroComponent,
+    PessoaCadastroComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule,
+    InputTextareaModule,
+    CalendarModule,
+    SelectButtonModule,
+    DropdownModule,
+    InputMaskModule,
+    CurrencyMaskModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
