157836e49773c6e42eef9bb423fd63fc73090858
Raulzito311 <raulmff@gmail.com>
2019-08-28 23:45:17 -0300

    Injetando serviços dentro de serviços e o decorador @Injectable

---
+++ Aula 15.07/src/app/funcionario-form/funcionario-form.component.ts
@@ -0,0 +1,21 @@
+import { LogService } from './../log.service';
+import { FuncionarioService } from './../funcionario.service';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionario-form',
+  templateUrl: './funcionario-form.component.html',
+  styleUrls: ['./funcionario-form.component.css']
+})
+export class FuncionarioFormComponent {
+
+  constructor(
+    private funcionarioService: FuncionarioService,
+    private logService: LogService
+  ) { }
+
+  adicionar(nome: string) {
+    this.funcionarioService.adicionar(nome);
+  }
+
+}
