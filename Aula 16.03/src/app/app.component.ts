bfb6833f3d8c00038c6fca3dbd00bccfabaa1f21
Raulzito311 <raulmff@gmail.com>
2019-09-01 13:35:45 -0300

    Fazendo requisição com GET e recebendo retorno

---
+++ Aula 16.03/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { CidadeService } from './cidade.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  cidades = [];
+
+  constructor(private cidadeService: CidadeService) { }
+
+  ngOnInit() {
+    this.cidadeService.consultar()
+      .then(cidades => {
+        this.cidades = cidades;
+      });
+  }
+
+  adicionar(nome: string) {
+    alert(nome);
+  }
+
+  excluir(id: number) {
+    alert(id);
+  }
+
+  atualizar(cidade: any) {
+    alert(JSON.stringify(cidade));
+  }
+
+}
