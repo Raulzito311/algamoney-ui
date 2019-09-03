cf998b291510c73a956db4db95fed49c2b5c9395
Raulzito311 <raulmff@gmail.com>
2019-09-02 21:39:35 -0300

    Fazendo requisição com PUT

---
+++ Aula 16.06/src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { CidadeService } from './cidade.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  cidades = [];
+
+  constructor(private cidadeService: CidadeService) { }
+
+  ngOnInit() {
+    this.consultar();
+  }
+
+  consultar() {
+    this.cidadeService.consultar()
+      .then(cidades => {
+        this.cidades = cidades;
+      });
+  }
+
+  adicionar(nome: string) {
+    this.cidadeService.adicionar({ nome })
+      .then(cidade => {
+        alert(`Cidade "${cidade.nome}" cadastrada com código ${cidade.id}`);
+        // this.cidades.push(cidade);
+        this.consultar();
+      });
+  }
+
+  excluir(id: number) {
+    this.cidadeService.excluir(id)
+      .then(() => {
+        alert('Cidade excluída com sucesso!');
+        this.consultar();
+      });
+  }
+
+  atualizar(cidade: any) {
+    this.cidadeService.atualizar(cidade)
+      .then(() => {
+        alert('Cidade alterada com sucesso!');
+      })
+  }
+
+}
