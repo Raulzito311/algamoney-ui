625fd371a5f8058f601901b3582e73e0f82112cd
Raulzito311 <raulmff@gmail.com>
2019-09-09 21:14:24 -0300

    Nova Aula

---
+++ Módulo 17/17.05- Implementando a paginação no serviço de lançamentos/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+
+import { LancamentoService, LancamentoFiltro } from './../lancamento.service';
+
+@Component({
+  selector: 'app-lancamentos-pesquisa',
+  templateUrl: './lancamentos-pesquisa.component.html',
+  styleUrls: ['./lancamentos-pesquisa.component.css']
+})
+export class LancamentosPesquisaComponent implements OnInit {
+
+  filtro = new LancamentoFiltro();
+
+  lancamentos = [];
+
+  cols = [
+    { field: 'pessoa', header: 'Pessoa' },
+    { field: 'descricao', header: 'Descricao' },
+    { field: 'dataVencimento', header: 'Vencimento' },
+    { field: 'dataPagamento', header: 'Pagamento' },
+    { field: 'valor', header: 'Valor' }
+  ];
+
+  constructor(private lancamentoService: LancamentoService) { }
+
+  ngOnInit() {
+    this.pesquisar();
+  }
+
+  pesquisar() {
+
+    this.lancamentoService.pesquisar(this.filtro)
+      .then(resultado => {
+        this.lancamentos = resultado.lancamentos;
+      });
+  }
+
+  getHeaderColumnClasses(header: string) {
+    if (header === 'Vencimento' || header === 'Pagamento') {
+      return 'col-data-header';
+    }
+    if (header === 'Valor') {
+      return 'col-valor-header';
+    }
+  }
+
+  getBodyColumnClasses(header: string, tipo: string) {
+    return header === 'Valor' ?
+      tipo === 'DESPESA' ?
+        ['redClass', 'col-valor-content'] : ['blueClass', 'col-valor-content'] :
+      header === 'Vencimento' || header === 'Pagamento' ?
+        'col-data-content' : null;
+  }
+
+}
