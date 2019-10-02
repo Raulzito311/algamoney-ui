349e0e75fa50cd024f042c555ec5fd4f71996864
Raulzito311 <raulmff@gmail.com>
2019-10-01 21:02:04 -0300

    Nova Aula

---
+++ Módulo 17/17.07- Desafio: criando a consulta e listagem de pessoas/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { PessoasModule } from './pessoas/pessoas.module';
+
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { LancamentoService } from './lancamentos/lancamento.service';
+import { PessoaService } from './pessoas/pessoa.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+
+    HttpClientModule,
+
+    CoreModule,
+
+    LancamentosModule,
+    PessoasModule
+  ],
+  providers: [
+    LancamentoService,
+    PessoaService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Módulo 17/17.07- Desafio: criando a consulta e listagem de pessoas/src/app/pessoas/pessoa.service.ts
@@ -0,0 +1,86 @@
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+export class PessoaFiltro {
+  nome: string;
+  pagina = 0;
+  itensPorPagina = 4;
+}
+
+export class Pessoa {
+  nome: string;
+  cidade: string;
+  estado: string;
+  ativo: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PessoaService {
+
+  pessoasUrl = 'http://localhost:8080/pessoas';
+
+  constructor(private http: HttpClient) { }
+
+  listarTodas(): Promise<any> {
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    return this.http.get(`${this.pessoasUrl}`, { headers })
+      .toPromise();
+  }
+
+  pesquisar(filtro: PessoaFiltro): Promise<any> {
+    let params = new HttpParams();
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    params = params.set('page', filtro.pagina.toString());
+    params = params.set('size', filtro.itensPorPagina.toString());
+
+    if (filtro.nome) {
+      params = params.set('nome', filtro.nome);
+    }
+
+    return this.http.get(`${this.pessoasUrl}`, { headers, params })
+      .toPromise()
+      .then(response => {
+        // this.getPessoas(response);
+        const pessoas = response['content'];
+
+        const resultado = {
+          pessoas,
+          total: response['totalElements']
+        };
+
+        return resultado;
+      });
+  }
+
+  getPessoas(response: any) {
+    const pessoas = [ ];
+    let pessoa: Pessoa;
+
+    const content = response['content'];
+
+    content.forEach(element => {
+      pessoa = new Pessoa();
+
+      pessoa.nome = element.nome;
+      pessoa.cidade = element['endereco'].cidade;
+      pessoa.estado = element['endereco'].estado;
+      pessoa.ativo = element.ativo;
+
+      pessoas.push(pessoa);
+
+    });
+
+    const resultado = {
+      pessoas,
+      total: response['totalElements']
+    };
+
+    return resultado;
+  }
+}
