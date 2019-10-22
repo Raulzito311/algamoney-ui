76bf58d1f1c2230b6404b5fcb9efc85c599b0cdd
Raulzito311 <raulmff@gmail.com>
2019-10-22 20:13:23 -0300

    Nova Aula

---
+++ Módulo 17/17.12- Criando um serviço de tratamento de erros/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -0,0 +1,101 @@
+import { ToastService } from './../../shared/toast.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { LazyLoadEvent, MessageService, ConfirmationService } from 'primeng/api';
+
+import { LancamentoService, LancamentoFiltro } from './../lancamento.service';
+
+import { Table } from 'primeng/table';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+
+@Component({
+  selector: 'app-lancamentos-pesquisa',
+  templateUrl: './lancamentos-pesquisa.component.html',
+  styleUrls: ['./lancamentos-pesquisa.component.css']
+})
+export class LancamentosPesquisaComponent implements OnInit {
+
+  filtro = new LancamentoFiltro();
+
+  totalRegistros = 0;
+
+  lancamentos = [];
+
+  @ViewChild('tabela', {static: true}) grid: Table;
+
+  cols = [
+    { field: 'pessoa', header: 'Pessoa' },
+    { field: 'descricao', header: 'Descricao' },
+    { field: 'dataVencimento', header: 'Vencimento' },
+    { field: 'dataPagamento', header: 'Pagamento' },
+    { field: 'valor', header: 'Valor' }
+  ];
+
+  constructor(
+    private lancamentoService: LancamentoService,
+    private toast: ToastService,
+    private confirmation: ConfirmationService,
+    private errorHandler: ErrorHandlerService
+  ) { }
+
+  ngOnInit() {  }
+
+  pesquisar(pagina = 0) {
+    this.filtro.pagina = pagina;
+
+    this.lancamentoService.pesquisar(this.filtro)
+      .then(resultado => {
+        this.totalRegistros = resultado.total;
+        this.lancamentos = resultado.lancamentos;
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  aoMudarPagina(event: LazyLoadEvent) {
+    const pagina = event.first / event.rows;
+    this.pesquisar(pagina);
+  }
+
+  confirmarExclusao(lancamento: any) {
+    this.confirmation.confirm({
+      message: 'Tem certeza que deseja excluír este lançamento?',
+      accept: () => {
+        this.excluir(lancamento);
+      }
+    });
+  }
+
+  excluir(lancamento: any) {
+    this.lancamentoService.excluir(lancamento.codigo)
+      .then(() => {
+        this.grid.reset();
+        this.toast.success('Lançamento excluído com sucesso');
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  toastf() {
+    this.toast.success('Lançamento excluído com sucesso');
+    this.toast.info('');
+    this.toast.warn('');
+    this.toast.error('');
+  }
+
+  getHeaderColumnClasses(header: string) {
+    if (header === 'Vencimento' || header === 'Pagamento') {
+      return 'col-data-header';
+    }
+    if (header === 'Valor') {
+      return 'col-valor-header';
+    }
+  }
+
+  getBodyColumnClasses(header: string, tipo: string) {
+    return header === 'Valor' ?
+      tipo === 'DESPESA' ?
+        ['redClass', 'col-valor-content'] : ['blueClass', 'col-valor-content'] :
+      header === 'Vencimento' || header === 'Pagamento' ?
+        'col-data-content' : null;
+  }
+
+}
---
+++ Módulo 17/17.12- Criando um serviço de tratamento de erros/src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+import { LazyLoadEvent } from 'primeng/components/common/api';
+
+import { PessoaFiltro, PessoaService } from '../pessoa.service';
+
+@Component({
+  selector: 'app-pessoas-pesquisa',
+  templateUrl: './pessoas-pesquisa.component.html',
+  styleUrls: ['./pessoas-pesquisa.component.css']
+})
+export class PessoasPesquisaComponent implements OnInit {
+
+  filtro = new PessoaFiltro();
+
+  totalRegistros = 0;
+
+  pessoas = [ ];
+
+  cols = [
+    { field: 'nome', header: 'Nome' },
+    { field: 'cidade', header: 'Cidade' },
+    { field: 'estado', header: 'Estado' },
+    { field: 'ativo', header: 'Status' }
+  ];
+
+  constructor(private pessoaService: PessoaService) { }
+
+  ngOnInit() {  }
+
+  pesquisar(pagina = 0) {
+    this.filtro.pagina = pagina;
+
+    this.pessoaService.pesquisar(this.filtro)
+      .then(resultado => {
+        this.totalRegistros = resultado.total;
+        this.pessoas = resultado.pessoas;
+      });
+  }
+
+  aoMudarPagina(event: LazyLoadEvent) {
+    const pagina = event.first / event.rows;
+    this.pesquisar(pagina);
+  }
+
+}
