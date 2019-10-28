bdbad712d7b2344557d21b6f5a6c265154497863
Raulzito311 <raulmff@gmail.com>
2019-10-27 23:04:31 -0300

    Nova Aula

---
+++ Módulo 17/17.15- Desafio: implementando a mudança de status de pessoas/src/app/core/core.module.ts
@@ -0,0 +1,46 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
+
+import localePt from '@angular/common/locales/pt';
+
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+
+import { MessageService, ConfirmationService } from 'primeng/api';
+
+import { ErrorHandlerService } from './error-handler.service';
+import { PessoaService } from '../pessoas/pessoa.service';
+import { LancamentoService } from '../lancamentos/lancamento.service';
+import { ToastService } from '../shared/toast.service';
+
+import { NavbarComponent } from './navbar/navbar.component';
+
+registerLocaleData(localePt);
+
+@NgModule({
+  declarations: [
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule,
+    ToastModule,
+    ConfirmDialogModule
+  ],
+  exports: [
+    NavbarComponent,
+    ToastModule,
+    ConfirmDialogModule
+  ],
+  providers: [
+    LancamentoService,
+    PessoaService,
+    MessageService,
+    ErrorHandlerService,
+
+    ToastService,
+    ConfirmationService,
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ]
+})
+export class CoreModule { }
