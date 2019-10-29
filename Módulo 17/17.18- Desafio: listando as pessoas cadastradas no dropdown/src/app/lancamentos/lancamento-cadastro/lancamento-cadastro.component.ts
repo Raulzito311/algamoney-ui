2fdc51a4d2c872f085fcb12639d1d581602faefb
Raulzito311 <raulmff@gmail.com>
2019-10-29 15:03:14 -0300

    Nova Aula

---
+++ Módulo 17/17.18- Desafio: listando as pessoas cadastradas no dropdown/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CategoriaService } from 'src/app/categorias/categoria.service';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+import { PessoaService } from 'src/app/pessoas/pessoa.service';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
+@Component({
+  selector: 'app-lancamento-cadastro',
+  templateUrl: './lancamento-cadastro.component.html',
+  styleUrls: ['./lancamento-cadastro.component.css']
+})
+export class LancamentoCadastroComponent implements OnInit {
+
+
+  tipos = [
+    { label: 'Receita', value: 'RECEITA' },
+    { label: 'Despesa', value: 'DESPESA' }
+  ];
+
+  categorias = [ ];
+
+  pessoas = [ ];
+
+  constructor(
+    private categoriaService: CategoriaService,
+    private pessoaService: PessoaService,
+    private errorHandler: ErrorHandlerService
+  ) { }
+
+  ngOnInit() {
+    this.carregarCategorias();
+    this.carregarPessoas();
+  }
+
+  carregarCategorias() {
+    return this.categoriaService.listarTodas()
+      .then(categorias => {
+          this.categorias = categorias.map(c => ({ label: c.nome, value: c.codigo }));
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  carregarPessoas() {
+    return this.pessoaService.listarTodas()
+      .then(pessoas => {
+          this.pessoas = pessoas.map(p => ({ label: p.nome, value: p.codigo }));
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+}
