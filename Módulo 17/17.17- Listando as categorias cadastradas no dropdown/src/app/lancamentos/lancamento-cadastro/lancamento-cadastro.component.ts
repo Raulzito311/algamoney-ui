3f41070a01d98ee304282e0599300b82db6a5326
Raulzito311 <raulmff@gmail.com>
2019-10-29 14:49:32 -0300

    Nova Aula

---
+++ Módulo 17/17.17- Listando as categorias cadastradas no dropdown/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CategoriaService } from 'src/app/categorias/categoria.service';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+
+@Component({
+  selector: 'app-lancamento-cadastro',
+  templateUrl: './lancamento-cadastro.component.html',
+  styleUrls: ['./lancamento-cadastro.component.css']
+})
+export class LancamentoCadastroComponent implements OnInit {
+
+
+  tipos = [
+    { label: 'Receita', value: 'RECEITA' },
+    { label: 'Despesa', value: 'DESPESA' }
+  ];
+
+  categorias = [
+    { label: 'Alimentação', value: 1 },
+    { label: 'Transporte', value: 2 }
+  ];
+
+  pessoas = [
+    { label: 'João da Silva', value: 1 },
+    { label: 'Sebastião Souza', value: 2 },
+    { label: 'Maria Abadia', value: 3 }
+  ];
+
+  constructor(
+    private categoriaService: CategoriaService,
+    private errorHandler: ErrorHandlerService
+  ) { }
+
+  ngOnInit() {
+    this.carregarCategorias();
+  }
+
+  carregarCategorias() {
+    return this.categoriaService.listarTodas()
+      .then(categorias => {
+          this.categorias = categorias.map(c => ({ label: c.nome, value: c.codigo }));
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+}
---
+++ Módulo 17/17.17- Listando as categorias cadastradas no dropdown/src/app/pessoas/pessoa.service.ts
@@ -0,0 +1,108 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+
+export class PessoaFiltro {
+  nome: string;
+  pagina = 0;
+  itensPorPagina = 4;
+}
+
+export class Pessoa {
+  codigo: number;
+  nome: string;
+  cidade: string;
+  estado: string;
+  ativo: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PessoaService {
+
+  pessoasUrl = 'http://localhost:8080/pessoas';
+
+  constructor(private http: HttpClient) { }
+
+  listarTodas(): Promise<any> {
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    return this.http.get(this.pessoasUrl, { headers })
+      .toPromise()
+      .then(response => response['content']);
+  }
+
+  pesquisar(filtro: PessoaFiltro): Promise<any> {
+    let params = new HttpParams();
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    params = params.set('page', filtro.pagina.toString());
+    params = params.set('size', filtro.itensPorPagina.toString());
+
+    if (filtro.nome) {
+      params = params.set('nome', filtro.nome);
+    }
+
+    return this.http.get(this.pessoasUrl, { headers, params })
+      .toPromise()
+      .then(response => {
+        // return this.getPessoas(response);
+        const pessoas = response['content'];
+
+        const resultado = {
+          pessoas,
+          total: response['totalElements']
+        };
+
+        return resultado;
+      });
+  }
+
+  excluir(codigo: number): Promise<void> {
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    return this.http.delete(`${this.pessoasUrl}/${codigo}`, { headers })
+      .toPromise()
+      .then(() => null);
+  }
+
+  alternarStatus(codigo: number, ativo: boolean): Promise<void> {
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==')
+      .append('Content-Type', 'application/json');
+
+    return this.http.put(`${this.pessoasUrl}/${codigo}/ativo`, ativo, { headers })
+      .toPromise()
+      .then(() => null);
+  }
+
+  getPessoas(response: any) {
+    const pessoas = [ ];
+    let pessoa: Pessoa;
+
+    const content = response['content'];
+
+    content.forEach(element => {
+      pessoa = new Pessoa();
+
+      pessoa.codigo = element.codigo;
+      pessoa.nome = element.nome;
+      pessoa.cidade = element['endereco'].cidade;
+      pessoa.estado = element['endereco'].estado;
+      pessoa.ativo = element.ativo;
+
+      pessoas.push(pessoa);
+
+    });
+
+    const resultado = {
+      pessoas,
+      total: response['totalElements']
+    };
+
+    return resultado;
+  }
+}
