39fa589891e1104a7f3dc0c9e2bda636cdb09dbf
Raulzito311 <raulmff@gmail.com>
2019-10-31 20:05:40 -0300

    Nova Aula

---
+++ Módulo 18/18.02- Configurando rotas na aplicação/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -0,0 +1,69 @@
+import { ToastService } from './../../shared/toast.service';
+import { LancamentoService } from './../lancamento.service';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { CategoriaService } from 'src/app/categorias/categoria.service';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+import { PessoaService } from 'src/app/pessoas/pessoa.service';
+
+import { Lancamento } from './../../core/model';
+
+@Component({
+  selector: 'app-lancamento-cadastro',
+  templateUrl: './lancamento-cadastro.component.html',
+  styleUrls: ['./lancamento-cadastro.component.css']
+})
+export class LancamentoCadastroComponent implements OnInit {
+
+
+  tipos = [
+    { label: 'Receita', value: 'RECEITA' },
+    { label: 'Despesa', value: 'DESPESA' }
+  ];
+
+  categorias = [ ];
+  pessoas = [ ];
+  lancamento = new Lancamento();
+
+  constructor(
+    private lancamentoService: LancamentoService,
+    private categoriaService: CategoriaService,
+    private pessoaService: PessoaService,
+    private toast: ToastService,
+    private errorHandler: ErrorHandlerService
+  ) { }
+
+  ngOnInit() {
+    this.carregarCategorias();
+    this.carregarPessoas();
+  }
+
+  salvar(form: FormControl) {
+    this.lancamentoService.adicionar(this.lancamento)
+      .then(() => {
+        this.toast.success('Lançamento adicionado com sucesso!');
+
+        form.reset();
+        this.lancamento = new Lancamento();
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  carregarCategorias() {
+    return this.categoriaService.listarTodas()
+      .then(categorias => {
+          this.categorias = categorias.map(c => ({ label: c.nome, value: c.codigo }));
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  carregarPessoas() {
+    return this.pessoaService.listarTodas()
+      .then(pessoas => {
+          this.pessoas = pessoas.map(p => ({ label: p.nome, value: p.codigo }));
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+}
