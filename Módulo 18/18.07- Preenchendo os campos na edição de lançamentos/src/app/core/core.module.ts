ea52d72c4c8e946a003f9ccb91ed193b6e86d3d0
Raulzito311 <raulmff@gmail.com>
2019-11-01 19:50:39 -0300

    Nova Aula

---
+++ Módulo 18/18.07- Preenchendo os campos na edição de lançamentos/src/app/core/core.module.ts
@@ -0,0 +1,52 @@
+import { RouterModule } from '@angular/router';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
+
+import localePt from '@angular/common/locales/pt';
+
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+
+import { MessageService, ConfirmationService } from 'primeng/api';
+
+import { ErrorHandlerService } from './error-handler.service';
+import { PessoaService } from '../pessoas/pessoa.service';
+import { LancamentoService } from '../lancamentos/lancamento.service';
+import { ToastService } from '../shared/toast.service';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { CategoriaService } from '../categorias/categoria.service';
+
+registerLocaleData(localePt);
+
+@NgModule({
+  declarations: [
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+
+    ToastModule,
+    ConfirmDialogModule
+  ],
+  exports: [
+    NavbarComponent,
+    ToastModule,
+    ConfirmDialogModule
+  ],
+  providers: [
+    LancamentoService,
+    PessoaService,
+    CategoriaService,
+
+    MessageService,
+    ErrorHandlerService,
+
+    ToastService,
+    ConfirmationService,
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ]
+})
+export class CoreModule { }
---
+++ Módulo 18/18.07- Preenchendo os campos na edição de lançamentos/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+
+import { LancamentoService } from './../lancamento.service';
+import { PessoaService } from 'src/app/pessoas/pessoa.service';
+import { CategoriaService } from 'src/app/categorias/categoria.service';
+import { ToastService } from './../../shared/toast.service';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+
+import { Lancamento } from './../../core/model';
+
+@Component({
+  selector: 'app-lancamento-cadastro',
+  templateUrl: './lancamento-cadastro.component.html',
+  styleUrls: ['./lancamento-cadastro.component.css']
+})
+export class LancamentoCadastroComponent implements OnInit {
+
+
+  tipos = [
+    { label: 'Receita', value: 'RECEITA' },
+    { label: 'Despesa', value: 'DESPESA' }
+  ];
+
+  categorias = [ ];
+  pessoas = [ ];
+  lancamento = new Lancamento();
+
+  constructor(
+    private lancamentoService: LancamentoService,
+    private categoriaService: CategoriaService,
+    private pessoaService: PessoaService,
+    private toast: ToastService,
+    private errorHandler: ErrorHandlerService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    const codigoLancamento = this.route.snapshot.params.codigo;
+
+    if (codigoLancamento) {
+      this.carregarLancamento(codigoLancamento);
+    }
+
+    this.carregarCategorias();
+    this.carregarPessoas();
+  }
+
+  get editando() {
+    return Boolean(this.lancamento.codigo);
+  }
+
+  salvar(form: FormControl) {
+    this.lancamentoService.adicionar(this.lancamento)
+      .then(() => {
+        this.toast.success('Lançamento adicionado com sucesso!');
+
+        form.reset();
+        this.lancamento = new Lancamento();
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  carregarCategorias() {
+    return this.categoriaService.listarTodas()
+      .then(categorias => {
+          this.categorias = categorias.map(c => ({ label: c.nome, value: c.codigo }));
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  carregarPessoas() {
+    return this.pessoaService.listarTodas()
+      .then(pessoas => {
+          this.pessoas = pessoas.map(p => ({ label: p.nome, value: p.codigo }));
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  carregarLancamento(codigo: number) {
+    this.lancamentoService.buscarPorCodigo(codigo)
+    .then(lancamento => {
+      this.lancamento = lancamento;
+    })
+    .catch(erro => this.errorHandler.handle(erro));
+  }
+
+}
