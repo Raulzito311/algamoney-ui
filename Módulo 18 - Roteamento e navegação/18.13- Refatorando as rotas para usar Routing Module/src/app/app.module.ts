13fb285a3b32c31076a573d3dbb6dbec525a89dd
Raulzito311 <raulmff@gmail.com>
2019-11-05 18:37:12 -0300

    Nova Aula

---
+++ Módulo 18 - Roteamento e navegação/18.13- Refatorando as rotas para usar Routing Module/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { PessoasModule } from './pessoas/pessoas.module';
+import { CoreModule } from './core/core.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+
+    HttpClientModule,
+
+    CoreModule,
+
+    LancamentosModule,
+    PessoasModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Módulo 18 - Roteamento e navegação/18.13- Refatorando as rotas para usar Routing Module/src/app/core/core.module.ts
@@ -0,0 +1,57 @@
+import { RouterModule } from '@angular/router';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
+
+import localePt from '@angular/common/locales/pt';
+
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+
+import { MessageService, ConfirmationService } from 'primeng/api';
+
+import { ErrorHandlerService } from './error-handler.service';
+import { PessoaService } from '../pessoas/pessoa.service';
+import { LancamentoService } from '../lancamentos/lancamento.service';
+import { ToastService } from '../shared/toast.service';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { CategoriaService } from '../categorias/categoria.service';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada.component';
+import { Title } from '@angular/platform-browser';
+
+registerLocaleData(localePt);
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    PaginaNaoEncontradaComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+
+    ToastModule,
+    ConfirmDialogModule
+  ],
+  exports: [
+    NavbarComponent,
+    ToastModule,
+    ConfirmDialogModule
+  ],
+  providers: [
+    // LancamentoService,
+    // PessoaService,
+    // CategoriaService,
+
+    // ErrorHandlerService,
+
+    MessageService,
+    // ToastService,
+    ConfirmationService,
+
+    // Title,
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ]
+})
+export class CoreModule { }
