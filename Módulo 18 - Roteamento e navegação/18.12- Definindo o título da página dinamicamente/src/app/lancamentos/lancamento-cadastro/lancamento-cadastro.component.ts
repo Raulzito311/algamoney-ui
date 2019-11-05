4ee96ebb79d076df194b5087c329b27c9d191f34
Raulzito311 <raulmff@gmail.com>
2019-11-05 15:11:09 -0300

    Nova Aula

---
+++ Módulo 18 - Roteamento e navegação/18.12- Definindo o título da página dinamicamente/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -0,0 +1,128 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
+import { LancamentoService } from './../lancamento.service';
+import { PessoaService } from 'src/app/pessoas/pessoa.service';
+import { CategoriaService } from 'src/app/categorias/categoria.service';
+import { ToastService } from './../../shared/toast.service';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+
+import { Lancamento } from './../../core/model';
+
+@Component({
+  selector: 'app-lancamento-cadastro',
+  templateUrl: './lancamento-cadastro.component.html',
+  styleUrls: ['./lancamento-cadastro.component.css']
+})
+export class LancamentoCadastroComponent implements OnInit {
+
+
+  tipos = [
+    { label: 'Receita', value: 'RECEITA' },
+    { label: 'Despesa', value: 'DESPESA' }
+  ];
+
+  categorias = [ ];
+  pessoas = [ ];
+  lancamento = new Lancamento();
+
+  constructor(
+    private lancamentoService: LancamentoService,
+    private categoriaService: CategoriaService,
+    private pessoaService: PessoaService,
+    private toast: ToastService,
+    private errorHandler: ErrorHandlerService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private title: Title
+  ) { }
+
+  ngOnInit() {
+    this.title.setTitle('Cadastro de Lançamentos');
+
+    const codigoLancamento = this.route.snapshot.params.codigo;
+
+    if (codigoLancamento) {
+      this.carregarLancamento(codigoLancamento);
+    }
+
+    this.carregarCategorias();
+    this.carregarPessoas();
+  }
+
+  get editando() {
+    return Boolean(this.lancamento.codigo);
+  }
+
+  salvar(form: FormControl) {
+    if (this.editando) {
+      this.atualizarLancamento(form);
+    } else {
+      this.adicionarLancamento(form);
+    }
+  }
+
+  adicionarLancamento(form: FormControl) {
+    this.lancamentoService.adicionar(this.lancamento)
+      .then(lancamentoAdicionado => {
+        this.toast.success('Lançamento adicionado com sucesso!');
+
+        // form.reset();
+        // this.lancamento = new Lancamento();
+        this.router.navigate(['/lancamentos', lancamentoAdicionado.codigo]);
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  atualizarLancamento(form: FormControl) {
+    this.lancamentoService.atualizar(this.lancamento)
+      .then(lancamento => {
+        this.toast.success('Lançamento atualizado com sucesso!');
+
+        this.lancamento = lancamento;
+
+        this.atualizarTitulo();
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  carregarCategorias() {
+    return this.categoriaService.listarTodas()
+      .then(categorias => {
+          this.categorias = categorias.map(c => ({ label: c.nome, value: c.codigo }));
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  carregarPessoas() {
+    return this.pessoaService.listarTodas()
+      .then(pessoas => {
+          this.pessoas = pessoas.map(p => ({ label: p.nome, value: p.codigo }));
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  carregarLancamento(codigo: number) {
+    this.lancamentoService.buscarPorCodigo(codigo)
+    .then(lancamento => {
+      this.lancamento = lancamento;
+
+      this.atualizarTitulo();
+    })
+    .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  novo(form: FormControl) {
+    form.reset();
+    this.lancamento = new Lancamento();
+
+    this.router.navigate(['/lancamentos/novo']);
+  }
+
+  atualizarTitulo() {
+    this.title.setTitle(`Edição de Lançamento: ${this.lancamento.descricao}`);
+  }
+
+}
