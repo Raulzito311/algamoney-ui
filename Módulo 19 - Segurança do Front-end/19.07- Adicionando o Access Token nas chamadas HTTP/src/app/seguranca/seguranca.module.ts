28c180a365b17dc7f23ad1e0601de741f2a93568
Raulzito311 <raulmff@gmail.com>
2019-11-09 22:39:48 -0300

    Nova Aula

---
+++ Módulo 19 - Segurança do Front-end/19.07- Adicionando o Access Token nas chamadas HTTP/src/app/seguranca/seguranca.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
+import { LoginFormComponent } from './login-form/login-form.component';
+
+import { SharedModule } from './../shared/shared.module';
+import { JwtModule } from '@auth0/angular-jwt';
+
+import { SegurancaRoutingModule } from './seguranca-routing.module';
+
+export function tokenGetter(): string {
+  return localStorage.getItem('token');
+}
+
+@NgModule({
+  declarations: [
+    LoginFormComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+
+    SharedModule,
+
+    InputTextModule,
+    ButtonModule,
+
+    JwtModule.forRoot({
+      config: {
+        tokenGetter,
+        whitelistedDomains: ['localhost:8080'],
+        blacklistedRoutes: ['http://localhost:8080/oauth/token']
+      }
+    }),
+
+    SegurancaRoutingModule
+  ]
+})
+export class SegurancaModule { }
