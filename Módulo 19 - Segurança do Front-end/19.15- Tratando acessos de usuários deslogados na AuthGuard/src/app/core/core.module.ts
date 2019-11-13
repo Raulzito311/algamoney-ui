fcc448c76f694506c806f20750305886bda4e14c
Raulzito311 <raulmff@gmail.com>
2019-11-13 15:18:24 -0300

    Nova Aula

---
+++ Módulo 19 - Segurança do Front-end/19.15- Tratando acessos de usuários deslogados na AuthGuard/src/app/core/core.module.ts
@@ -0,0 +1,64 @@
+import { RouterModule } from '@angular/router';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
+
+import localePt from '@angular/common/locales/pt';
+
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+
+import { MessageService, ConfirmationService } from 'primeng/api';
+
+import { ErrorHandlerService } from './error-handler.service';
+import { PessoaService } from '../pessoas/pessoa.service';
+import { LancamentoService } from '../lancamentos/lancamento.service';
+import { ToastService } from '../shared/toast.service';
+import { AuthService } from './../seguranca/auth.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { CategoriaService } from '../categorias/categoria.service';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada.component';
+import { NaoAutorizadoComponent } from './nao-autorizado.component';
+import { Title } from '@angular/platform-browser';
+
+registerLocaleData(localePt);
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    PaginaNaoEncontradaComponent,
+    NaoAutorizadoComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+
+    ToastModule,
+    ConfirmDialogModule
+  ],
+  exports: [
+    NavbarComponent,
+    ToastModule,
+    ConfirmDialogModule
+  ],
+  providers: [
+    // LancamentoService,
+    // PessoaService,
+    // CategoriaService,
+
+    // ErrorHandlerService,
+
+    // AuthService,
+    // JwtHelperService
+
+    MessageService,
+    // ToastService,
+    ConfirmationService,
+
+    // Title,
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ]
+})
+export class CoreModule { }
---
+++ Módulo 19 - Segurança do Front-end/19.15- Tratando acessos de usuários deslogados na AuthGuard/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -0,0 +1,99 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+import { LazyLoadEvent, ConfirmationService } from 'primeng/api';
+import { Table } from 'primeng/table';
+
+import { LancamentoService, LancamentoFiltro } from './../lancamento.service';
+import { ToastService } from './../../shared/toast.service';
+import { AuthService } from './../../seguranca/auth.service';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+
+@Component({
+  selector: 'app-lancamentos-pesquisa',
+  templateUrl: './lancamentos-pesquisa.component.html',
+  styleUrls: ['./lancamentos-pesquisa.component.css']
+})
+export class LancamentosPesquisaComponent implements OnInit {
+
+  filtro = new LancamentoFiltro();
+
+  totalRegistros = 0;
+
+  lancamentos = [];
+
+  @ViewChild('tabela', {static: true}) grid: Table;
+
+  cols = [
+    { field: 'pessoa', header: 'Pessoa' },
+    { field: 'descricao', header: 'Descricao' },
+    { field: 'dataVencimento', header: 'Vencimento' },
+    { field: 'dataPagamento', header: 'Pagamento' },
+    { field: 'valor', header: 'Valor' }
+  ];
+
+  constructor(
+    private lancamentoService: LancamentoService,
+    private auth: AuthService,
+    private errorHandler: ErrorHandlerService,
+    private toast: ToastService,
+    private confirmation: ConfirmationService,
+    private title: Title
+  ) { }
+
+  ngOnInit() {
+    this.title.setTitle('Pesquisa de Lançamentos');
+  }
+
+  pesquisar(pagina = 0) {
+    this.filtro.pagina = pagina;
+
+    this.lancamentoService.pesquisar(this.filtro)
+      .then(resultado => {
+        this.totalRegistros = resultado.total;
+        this.lancamentos = resultado.lancamentos;
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  aoMudarPagina(event: LazyLoadEvent) {
+    const pagina = event.first / event.rows;
+    this.pesquisar(pagina);
+  }
+
+  confirmarExclusao(lancamento: any) {
+    this.confirmation.confirm({
+      message: 'Tem certeza que deseja excluír este lançamento?',
+      accept: () => {
+        this.excluir(lancamento);
+      }
+    });
+  }
+
+  excluir(lancamento: any) {
+    this.lancamentoService.excluir(lancamento.codigo)
+      .then(() => {
+        this.grid.reset();
+        this.toast.success('Lançamento excluído com sucesso');
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  getHeaderColumnClasses(header: string) {
+    if (header === 'Vencimento' || header === 'Pagamento') {
+      return 'col-data-header';
+    }
+    if (header === 'Valor') {
+      return 'col-valor-header';
+    }
+  }
+
+  getBodyColumnClasses(header: string, tipo: string) {
+    return header === 'Valor' ?
+      tipo === 'DESPESA' ?
+        ['redClass', 'col-valor-content'] : ['blueClass', 'col-valor-content'] :
+      header === 'Vencimento' || header === 'Pagamento' ?
+        'col-data-content' : null;
+  }
+
+}
