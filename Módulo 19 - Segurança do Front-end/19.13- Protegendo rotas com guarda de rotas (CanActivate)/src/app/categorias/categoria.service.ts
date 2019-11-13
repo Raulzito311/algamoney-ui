83cdcbd34a93f1b7853d1dcee39f5d5427ce8d16
Raulzito311 <raulmff@gmail.com>
2019-11-13 14:27:44 -0300

    Nova Aula

---
+++ Módulo 19 - Segurança do Front-end/19.13- Protegendo rotas com guarda de rotas (CanActivate)/src/app/categorias/categoria.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  categoriasUrl = 'http://localhost:8080/categorias';
+
+  constructor(private http: HttpClient) { }
+
+  listarTodas(): Promise<any> {
+    return this.http.get(this.categoriasUrl)
+      .toPromise();
+  }
+
+}
